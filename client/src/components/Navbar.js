7f706731a7b98d6a9cc453aa1f5684abf4d4bb06
LeavesEatingCow <69094343+LeavesEatingCow@users.noreply.github.com>
2023-11-17 02:39:46 -0500

    Updated Navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,20 +1,20 @@
-import React, { useState } from "react";
-import Logo from "../Assets/Logo.svg";
-import { BsCart2 } from "react-icons/bs";
-import { HiOutlineBars3 } from "react-icons/hi2";
-import Box from "@mui/material/Box";
-import Drawer from "@mui/material/Drawer";
-import List from "@mui/material/List";
-import Divider from "@mui/material/Divider";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import HomeIcon from "@mui/icons-material/Home";
-import InfoIcon from "@mui/icons-material/Info";
-import CommentRoundedIcon from "@mui/icons-material/CommentRounded";
-import PhoneRoundedIcon from "@mui/icons-material/PhoneRounded";
-import ShoppingCartRoundedIcon from "@mui/icons-material/ShoppingCartRounded";
+import React, { useState } from "react"
+import Logo from "../Assets/Logo.svg"
+import { BsCart2 } from "react-icons/bs"
+import { HiOutlineBars3 } from "react-icons/hi2"
+import Box from "@mui/material/Box"
+import Drawer from "@mui/material/Drawer"
+import List from "@mui/material/List"
+import Divider from "@mui/material/Divider"
+import ListItem from "@mui/material/ListItem"
+import ListItemButton from "@mui/material/ListItemButton"
+import ListItemIcon from "@mui/material/ListItemIcon"
+import ListItemText from "@mui/material/ListItemText"
+import HomeIcon from "@mui/icons-material/Home"
+import InfoIcon from "@mui/icons-material/Info"
+import CommentRoundedIcon from "@mui/icons-material/CommentRounded"
+import PhoneRoundedIcon from "@mui/icons-material/PhoneRounded"
+import ShoppingCartRoundedIcon from "@mui/icons-material/ShoppingCartRounded"
 
 const Navbar = () => {
 
